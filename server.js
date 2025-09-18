1e8822b0b24fc697f269dbc3db804707087e7e65
cristaldafinny <cdso2@aluno.ifnmg.edu.br>
2025-09-18 19:51:27 +0000

    mensgaem de commit

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria endpoint na rota da raiz do projeto
+app.get("/", async (req, res) => {        // Cria endpoint na rota da raiz do projeto
   console.log("Rota GET / solicitada");
+  
+  const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
+
+let dbStatus = "ok";
+try {
+  await db.query("SELECT 1");
+} catch (e) {
+  dbStatus = e.message;
+}
   res.json({
 		message: "API para coisas",      // Substitua pelo conteúdo da sua API
     author: "Cristal Dafinny Sousa de Oliveira",    // Substitua pelo seu nome
+      statusBD: dbStatus   // Verifica o BD
   });
 });
 
